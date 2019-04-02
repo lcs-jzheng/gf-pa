7c8a989e81b16e4da3250951d16d62cd16854d87
Jeff Zheng <jzheng@lcs.on.ca>
2019-04-02 14:08:45 -0400

    Added photos for the background and the actor car

---
+++ MY GAME/RaceTrack.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class RaceTrack here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class RaceTrack extends World
+{
+
+    /**
+     * Constructor for objects of class RaceTrack.
+     * 
+     */
+    public RaceTrack()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(500, 500, 1); 
+    }
+}
